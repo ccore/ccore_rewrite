2692abafde55722b8deca3b99ba14561fc574908
Thomas Versteeg <thomasversteeg@gmx.com>
2016-06-03 16:11:11 +0200

    Fix installation of the build files and add debian building

--- src/cc_event.c
+++ src/cc_event.c
@@ -5,12 +5,8 @@
 
 #include <stdlib.h>
 
-#ifdef CC_DISABLE_THREADS
 static struct cc_event _event_stack[CC_EVENT_MAX_STACK_SIZE];
 static int _event_stack_pointer;
-#else
-#error "TODO, implement thread-safe event pool"
-#endif
 
 /* Private functions */
 
@@ -18,7 +14,6 @@ static int _event_stack_pointer;
 
 int cc_push_event(struct cc_event event)
 {
-#ifdef CC_DISABLE_THREADS
 	if(_event_stack_pointer == CC_EVENT_MAX_STACK_SIZE - 1){
 		cc_set_error("The event stack is full, call \"cc_pop_event\" (maximum \"%d\" items)", CC_EVENT_MAX_STACK_SIZE);
 		return 0;
@@ -27,16 +22,12 @@ int cc_push_event(struct cc_event event)
 	_event_stack[_event_stack_pointer] = event;
 
 	return 1;
-#else
-	return 0;
-#endif
 }
 
 struct cc_event cc_pop_event(void)
 {
 	struct cc_event event;
 
-#ifdef CC_DISABLE_THREADS
 	if(_event_stack_pointer == -1){
 		event.type = CC_EVENT_SKIP;
 		return event;
@@ -46,16 +37,9 @@ struct cc_event cc_pop_event(void)
 	_event_stack_pointer--;
 
 	return event;
-#else
-	return NULL;
-#endif
 }
 
 void cc_clear_event_queue(void)
 {
-#ifdef CC_DISABLE_THREADS
 	_event_stack_pointer = -1;
-#else
-	return 0;
-#endif
 }
